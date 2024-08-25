9dc5d16f4b2164e5700c941968eabb47fe4a99a9
Benjamin Hospodar <Benjamin.hospodar@gmail.com>
2024-08-25 00:54:43 -0400

    updated database code

--- database-initializer.js
+++ database-initializer.js
@@ -1,37 +1,13 @@
 import { MongoClient } from "mongodb";
 
-import art from './artwork/gallery.json' assert { type: "json" };
-//will lose marks, change
-//eithout hardcoding, use mongoose?????
+import acounts from './Accounts/initAccounts.json' assert { type: "json" };
+import items from './Inventory/items.json' assert { type: "json" };
 
-let artists = [];
+console.log(acounts);
+console.log("^ accounts ^");
 
-art.forEach(artKey => {
-  let artist = null;
-
-  // Check if artist already exists
-  for (let i = 0; i < artists.length; i++) {
-    if (artists[i].name === artKey.Artist) {
-      artist = artists[i];
-      break;
-    }
-  }
-
-  if (artist) {
-    // Artist found, add artwork to existing artist
-    artist.artworks.push(artKey.Title);
-  } else {
-    // Artist not found, create a new artist
-    artist = {
-      name: artKey.Artist,
-      artworks: [artKey.Title]
-    };
-
-    artists.push(artist);
-  }
-});
-
-console.log(artists);
+console.log(items);
+console.log("^ items ^");
 
 const uri = "mongodb://127.0.0.1:27017/";
 const client = new MongoClient(uri);
